940040f14d0e3232f6619f5993d2c4b167a7ad74
Justin Brown <justinbrown@Justins-MacBook-Pro.local>
2018-05-07 01:26:53 -0700

    initial commit

---
+++ gulpfile.js
@@ -0,0 +1,53 @@
+const gulp = require('gulp'),
+    plumber = require('gulp-plumber'),
+    rename = require('gulp-rename');
+const autoprefixer = require('gulp-autoprefixer');
+const babel = require('gulp-babel');
+const sass = require('gulp-sass');
+const browserSync = require('browser-sync');
+
+gulp.task('browser-sync', () => {
+  browserSync({
+    server: {
+       baseDir: "./"
+    }
+  });
+});
+
+gulp.task('bs-reload', () => {
+  browserSync.reload();
+});
+
+
+gulp.task('styles', () => {
+  gulp.src(['src/scss/**/*.scss'])
+    .pipe(plumber({
+      errorHandler: (error) => {
+        console.log(error.message);
+        this.emit('end');
+    }}))
+    .pipe(sass())
+    .pipe(autoprefixer('last 10 versions'))
+    .pipe(gulp.dest('css/'))
+    .pipe(browserSync.reload({stream:true}))
+});
+
+gulp.task('scripts', () => {
+  return gulp.src('src/js/**/*.js')
+    .pipe(plumber({
+      errorHandler: (error) => {
+        console.log(error.message);
+        this.emit('end');
+    }}))
+    .pipe(babel({
+      presets: ['env']
+    }))
+    .pipe(gulp.dest('js/'))
+    .pipe(browserSync.reload({stream:true}))
+});
+
+gulp.task('default', ['browser-sync'], () => {
+  gulp.watch("src/scss/**/*.scss", ['styles']);
+  gulp.watch("src/js/**/*.js", ['scripts']);
+  gulp.watch("*.html", ['bs-reload']);
+});
